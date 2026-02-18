3bd3bc695c1e69c5829b9d948911a21217cab14a
Kryen112 <chancemantracker@gmail.com>
2026-02-18 08:30:19 +0100

    Gabooty medium clue step requires Jungle Potion

--- docs/assets/items-BWF3thbR.js
+++ docs/assets/items-BWF3thbR.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-D5Tc1oWf.js";async function t(){return await e.ensureItemsLoaded(),`
+import{f as e}from"./index-D-SGbWpC.js";async function t(){return await e.ensureItemsLoaded(),`
         <div class="items-header">
             <h1>Items</h1>
             <button id="filter-overrides-toggle" title="Toggle filters and overrides">
--- docs/assets/reportABug-Cy7lssiN.js
+++ docs/assets/reportABug-Cy7lssiN.js
@@ -1,4 +1,4 @@
-import{f as n}from"./index-D5Tc1oWf.js";let o=null,t=null;async function d(){return`
+import{f as n}from"./index-D-SGbWpC.js";let o=null,t=null;async function d(){return`
         <h1>Report a bug</h1>
         <p>Report your problems here. It will automatically also send your obtained and rolled for me to check :)</p>
         <textarea id="bugText" placeholder="Describe the bug..." cols="50" rows="10"></textarea><br/><br/>
