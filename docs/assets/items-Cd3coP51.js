5b0241c716df7df92b98d2e9f091df513264c925
Claaaaaay2 <129789716+Claaaaaay2@users.noreply.github.com>
2026-08-26 12:42:00 -0700

    added grog to reward list

--- docs/assets/items-Cd3coP51.js
+++ docs/assets/items-Cd3coP51.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-BOEcAN3E.js";async function t(){return await e.ensureItemsLoaded(),`
+import{f as e}from"./index-BBzXYqsG.js";async function t(){return await e.ensureItemsLoaded(),`
         <div class="items-header">
             <h1>Items</h1>
             <button id="filter-overrides-toggle" title="Toggle filters and overrides">
